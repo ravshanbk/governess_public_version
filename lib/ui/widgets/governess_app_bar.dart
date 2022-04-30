b8e3bf0cecd85ec6cd6e20a78174256f102c2f40
ravshanbk <ravshanbekxojamuratov65@gmail.com>
2022-04-30 19:19:36 +0500

    Logo consideration

--- lib/ui/widgets/governess_app_bar.dart
+++ lib/ui/widgets/governess_app_bar.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:governess/consts/size_config.dart';
 
 PreferredSizeWidget get governessAppBar => AppBar(
-      leading: const Image(
-        image: AssetImage("assets/images/governessLogo.png"),
+      leadingWidth: gW(80.0),
+      leading: Padding(
+        padding: EdgeInsets.only(left: gW(20.0)),
+        child: Image(
+          image: const AssetImage("assets/images/governessLogo.png"),
+          height: gH(80.0),
+        ),
       ),
       backgroundColor: mainColor,
       elevation: 0,
